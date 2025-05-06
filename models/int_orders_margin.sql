e0e06c370d2c6cedfa66e877231824363c18705a
JimenaRR <jimenarenteriaramos@gmail.com>
2025-05-06 14:18:55 +0000

    extra margin

---
+++ models/int_orders_margin.sql
@@ -0,0 +1,11 @@
+SELECT
+    orders_id
+    ,date_date
+    ,ROUND(SUM(revenue),2) AS revenue
+    ,SUM(quantity) AS quantity
+    ,ROUND(SUM(purchase_cost),2) AS purchase_cost
+    ,ROUND(((SUM(revenue)) - (SUM(purchase_cost))),2) AS margin
+FROM {{ref("int_sales_margin")}}
+GROUP BY orders_id, date_date
+ORDER BY date_date DESC
+
