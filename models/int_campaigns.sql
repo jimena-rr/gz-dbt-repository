b05aab3a6ee0d279fdd69919197d43e1bff20adc
JimenaRR <jimenarenteriaramos@gmail.com>
2025-05-07 15:12:54 +0000

    last challlenge

--- models/int_campaigns.sql
+++ models/int_campaigns.sql
@@ -1,3 +1,8 @@
+{{ dbt_utils.union_relations(
+    relations=[ref('stg_raw__adwords'), source('my_source', 'my_table')],
+    exclude=["_loaded_at"]
+) }}
+
 SELECT * FROM {{ ref('stg_raw__adwords') }}
 UNION ALL
 SELECT * FROM {{ ref('stg_raw__bing') }}
