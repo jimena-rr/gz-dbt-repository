7cc27b8a299e13687fbae2cbae46eb34077b2837
JimenaRR <jimenarenteriaramos@gmail.com>
2025-05-07 12:05:04 +0000

    again

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,20 +1,21 @@
 SELECT
     c.date_date
     ,f.operational_margin - c.ads_cost AS ads_margin
--- average_basket
--- operational_margin
--- ads_cost
--- ads_impression
--- ads_clicks
--- quantity
--- revenue
--- purchase_cost
--- margin
--- shipping_fee
--- logcost
--- ship_cost
+    ,average_basket
+    ,operational_margin
+    ,ads_cost
+    ,ads_impressions
+    ,ads_clicks
+    ,quantity
+    ,revenue
+    ,purchase_cost
+    ,margin
+    ,shipping_fee
+    ,logcost
+    ,ship_cost
 FROM {{ref('int_campaigns_day')}} AS c
 FULL OUTER JOIN {{ref('finance_days')}} AS f
 ON c.date_date = f.date_date
 ORDER BY date_date DESC
+
 
